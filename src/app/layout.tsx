cc0ff9fd87a964829ac7fc996d0299553b72c1f6
Gmayer111 <gaelmayer@yahoo.fr>
2024-02-06 19:42:12 +0100

    feat: add pages path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "../../global.css";
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Restaurant Van Long !</div>;
+  return <div>Restaurant Van Long</div>;
 }
