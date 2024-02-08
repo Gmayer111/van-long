0ae5218863c8ce850a53defe8933bd09aa981086
Gmayer111 <gaelmayer@yahoo.fr>
2024-02-08 18:52:28 +0100

    feat: creating navbar & add logo

--- src/components/layout/header.component.tsx
+++ src/components/layout/header.component.tsx
@@ -2,16 +2,20 @@ import Image from "next/image";
 import React from "react";
 import HeaderImg from "public/assets/img/header-img.jpg";
 import { merriweather } from "src/utils/font";
+import { NavComponent } from "./nav/nav.component";
 
 const HeaderComponent = () => {
   return (
     <header>
-      <div className={`${merriweather.className} headerImgContainer`}>
-        <Image src={HeaderImg} alt="Image de l'en-tête" />
-        <div className="mainTitleContainer">
-          <h1>RESTAURANT VAN LONG</h1>
+      <div>
+        <NavComponent />
+        <div className={`${merriweather.className} headerImgContainer`}>
+          <Image src={HeaderImg} alt="Image de l'en-tête" />
+          <div>
+            <h1>RESTAURANT VAN LONG</h1>
+          </div>
+          <span>Cuisine vietnamienne traditionnelle</span>
         </div>
-        <span>Cuisine vietnamienne traditionnelle</span>
       </div>
     </header>
   );
