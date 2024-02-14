51be174eac0f4e8e4c9ea854ce6cea91e3d14591
Gmayer111 <gaelmayer@yahoo.fr>
2024-02-14 15:42:04 +0100

    feat: wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,24 @@ export default function Home() {
           <Image src={BambooImage} alt="Logo du restaurant" />
         </div>
       </section>
+      <section className="topSectionHomePageContainer">
+        <div>
+          <Image src={BambooImage} alt="Logo du restaurant" />
+        </div>
+        <div>
+          <p className={merriweather.className}>
+            Situé dans le 13ème arrondissement de Paris depuis 1992, de la rue
+            Pascal à la rue du Champ de l’Alouette, notre restaurant
+            traditionnel vietnamien vous accueille dans un cadre simple et
+            chaleureux. Tous nos plats sont élaborés sur place, à partir de
+            produits frais, et vous rappelleront les saveurs subtiles de la
+            cuisine vietnamienne.
+          </p>
+        </div>
+        <div>
+          <Image src={BambooImage} alt="Logo du restaurant" />
+        </div>
+      </section>
     </>
   );
 }
