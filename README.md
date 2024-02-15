8f59a2316ef83aff684f3e333d9585f5994c5644
Gaël Mayer <46091558+Gmayer111@users.noreply.github.com>
2024-02-15 11:22:40 +0100

    feat: creating carousel & add pictures (#8)

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Restaurant Van Long
+
+Van Long restaurant Website located in Paris, 13th district. This website allow to display the menu and his location.
+
+## Problem
+
+Currently built with Wordpress, the aim of this development is to migrate to NextJS.
+
+## Tech stack
+
+- NextJS
+- Yarn
+
+## How to run
+
+1. check node version, sould be >= node v18
+2. yarn install && yarn dev
+
+## Actual URL Link
+
+[van-long.fr](https://van-long.fr/)
---
+++ src/components/carousel/carousel.component.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Carousel } from "react-responsive-carousel";
+import BoatImage from "public/assets/img/boat.jpg";
+import EntranceImage from "public/assets/img/entrance.jpg";
+import LanternesImage from "public/assets/img/lanternes.jpg";
+import PaintImage from "public/assets/img/paint.jpg";
+import RoomOneImage from "public/assets/img/room.jpg";
+import RoomImage from "public/assets/img/room-1.jpg";
+import RoomTwoImage from "public/assets/img/room-2.jpg";
+import { useWindowSize } from "src/utils/windowSize";
+
+const CarouselComponent = () => {
+  const size = useWindowSize();
+  const carouselItems = [
+    EntranceImage,
+    BoatImage,
+    LanternesImage,
+    PaintImage,
+    RoomImage,
+    RoomOneImage,
+    RoomTwoImage,
+  ];
+  return (
+    <Carousel
+      centerMode={size.width < 768 ? false : true}
+      infiniteLoop
+      showStatus={false}
+    >
+      {carouselItems.map((carouselItem, index) => (
+        <div>
+          <Image src={carouselItem} alt={`Photo ${index} du restaurant`} />
+        </div>
+      ))}
+    </Carousel>
+  );
+};
+
+export default CarouselComponent;
