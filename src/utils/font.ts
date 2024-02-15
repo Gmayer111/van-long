04cb060f8fbf625afd7fd56f693e7ebfb35383dc
Gmayer111 <gaelmayer@yahoo.fr>
2024-02-15 20:17:42 +0100

    fix: refacto top section

--- src/utils/font.ts
+++ src/utils/font.ts
@@ -3,11 +3,11 @@ import { Merriweather, Merriweather_Sans } from "next/font/google";
 export const merriweather = Merriweather({
   subsets: ["latin"],
   display: "swap",
-  weight: "300",
+  weight: ["300", "400", "700"],
 });
 
 export const merriweather_Sans = Merriweather_Sans({
   subsets: ["latin"],
   display: "swap",
-  weight: "300",
+  weight: ["300", "400", "500"],
 });
