5404cc5507ec7df4d36f1bbf6b5f392924132c73
Gmayer111 <gaelmayer@yahoo.fr>
2024-02-28 16:28:06 +0100

    fix: remove height top section & add br element to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,13 +43,13 @@ export default function Home() {
               Situé dans le 13ème arrondissement de Paris depuis 1992, de la rue
               Pascal à la rue du Champ de l’Alouette, notre restaurant
               traditionnel vietnamien vous accueille dans un cadre simple et
-              chaleureux. Tous nos plats sont élaborés sur place, à partir de
-              produits frais, et vous rappelleront les saveurs subtiles de la
-              cuisine vietnamienne.
+              chaleureux. <br /> Tous nos plats sont élaborés sur place, à
+              partir de produits frais, et vous rappelleront les saveurs
+              subtiles de la cuisine vietnamienne.
             </p>
           </div>
         </section>
-        <section className="bottomSectionHomePageContainer">
+        <section>
           <CarouselComponent />
         </section>
       </div>
