ed1fa8a9ed795bcb8a4cf0d62e1a42a1bb8c3580
Gmayer111 <gaelmayer@yahoo.fr>
2024-03-04 11:30:19 +0100

    fix: resolve svg syntaxt & key inside map & priority img

--- public/assets/icons/close-icon.tsx
+++ public/assets/icons/close-icon.tsx
@@ -5,14 +5,14 @@ const CloseIcon = ({ iconStroke = "white" }: { iconStroke?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
-    stroke-width="1.5"
+    strokeWidth="1.5"
     stroke={iconStroke}
     width={"35px"}
     height={"35px"}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M6 18 18 6M6 6l12 12"
     />
   </svg>
