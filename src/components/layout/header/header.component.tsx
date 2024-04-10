9725ccaf26f47a809b04c6ddcb84cba9a6bb984a
Gmayer111 <gaelmayer@yahoo.fr>
2024-04-10 21:56:48 +0200

    fix: adding header title component

--- src/components/layout/header/header.component.tsx
+++ src/components/layout/header/header.component.tsx
@@ -1,23 +1,20 @@
-"use client";
 import Image from "next/image";
 import React from "react";
 import HeaderImg from "public/assets/images/header-img.jpg";
 import { merriweather } from "src/utils/font";
-import { NavComponent } from "./nav/nav.component";
-import HeaderTitleComponent from "./header/header-title.component";
-import { usePathname } from "next/navigation";
-import { HeaderTitles } from "src/types/header";
+import { NavComponent } from "../nav/nav.component";
 
 const HeaderComponent = () => {
-  const currentPath = usePathname();
-
   return (
     <header>
       <div>
         <NavComponent />
         <div className={`${merriweather.className} headerImgContainer`}>
           <Image priority={true} src={HeaderImg} alt="Image de l'en-tête" />
-          <HeaderTitleComponent headerTitle={HeaderTitles[currentPath]} />
+          <div>
+            <h1>RESTAURANT VAN LONG</h1>
+          </div>
+          <span>Cuisine vietnamienne traditionnelle</span>
         </div>
       </div>
     </header>
