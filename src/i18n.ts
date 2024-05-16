987d42063e4420c7804c779baf478aa3ae530a2c
Gmayer111 <gaelmayer@yahoo.fr>
2024-05-16 11:24:54 +0200

    feat: move messages files & add menu translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -9,6 +9,9 @@ export default getRequestConfig(async ({ locale }) => {
   if (!locales.includes(locale as any)) notFound();
 
   return {
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages: {
+      ...(await import(`../messages/${locale}/${locale}.json`)).default,
+      ...(await import(`../messages/${locale}/menu.json`)).default,
+    },
   };
 });
