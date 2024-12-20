4058b3df4b4f174e623d938487d8838b02bfec90
Gmayer111 <gaelmayer@yahoo.fr>
2024-12-20 10:16:49 +0100

    feat:create tab with styles

---
+++ src/components/tab/tab.component.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+
+export type TTabProps = {
+  tabItems: TTabItem[];
+};
+
+type TTabItem = {
+  content: string;
+  path: string;
+};
+
+const Tab = ({ tabItems }: TTabProps) => {
+  return (
+    <ul className="tab-container">
+      {tabItems.map((content, index) => (
+        <li className="tab-list-constainer" key={`${content}-${index}`}>
+          <Link href={content.path}>{content.content}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Tab;
