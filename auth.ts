aa19c9eef9fe8f8491dc119f0ee8e01ef562f818
Gmayer111 <gaelmayer@yahoo.fr>
2025-02-05 16:25:40 +0100

    feat: add prisma adaptater & nodemailer with basic config

--- auth.ts
+++ auth.ts
@@ -1,5 +1,14 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
+import prisma from "src/lib/db";
+import NodeMailer from "next-auth/providers/nodemailer";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    NodeMailer({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 });
