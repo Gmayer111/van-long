d51afc8ce7881aca7990d36f13da9b17528074d1
Gmayer111 <gaelmayer@yahoo.fr>
2025-02-20 23:08:29 +0100

    feat: create user & verification token actions with tests

---
+++ src/app/actions/users/user.actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { Prisma } from "@prisma/client";
+import prisma from "src/lib/db";
+
+export async function getUserByEmail(email: string) {
+  try {
+    const result = await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    return { result };
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2001") {
+        return { error: "User with this email does not exists" };
+      }
+    }
+    return { error: "Unknown error" };
+  }
+}
