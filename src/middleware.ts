8fab44d37c698f8f10436df8c73052bf61cfa0c3
Gmayer111 <gaelmayer@yahoo.fr>
2025-03-17 23:52:19 +0100

    feat: update user auth condition with cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,13 @@ import { cookies } from "next/headers";
 import { decrypt } from "./lib/sessions";
 
 export default async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
   const path = request.nextUrl.pathname;
   const isProtectedRoute = path.includes("admin");
-  const cookieToken = (await cookies()).get("session")?.value;
-  const session = await decrypt(cookieToken);
+  const currentSession = await cookies().get("session")?.value;
+  const session = await decrypt(currentSession);
   const hasEqualToken =
-    request.nextUrl.searchParams.get("token") === cookieToken;
+    request.nextUrl.searchParams.get("token") === currentSession;
 
   if (isProtectedRoute && !session?.userId) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
@@ -21,7 +22,12 @@ export default async function middleware(request: NextRequest) {
     hasEqualToken &&
     request.nextUrl.pathname.startsWith("/fr/admin")
   ) {
-    return NextResponse.next();
+    response.cookies.set({
+      name: "navSession",
+      value: "logged",
+      path: "/fr",
+    });
+    return response;
   }
 
   const defaultLocale = request.cookies.get("NEXT_LOCALE")?.value || "fr";
