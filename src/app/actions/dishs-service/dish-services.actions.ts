6e4ceb884895816f74105c933add52a37cf28d85
Gmayer111 <gaelmayer@yahoo.fr>
2025-04-15 10:10:37 +0200

    feat: update dish services with extras table incl tests

--- src/app/actions/dishs-service/dish-services.actions.ts
+++ src/app/actions/dishs-service/dish-services.actions.ts
@@ -5,14 +5,16 @@ export async function getAllDishServices() {
   return await prisma.dishService.findMany({});
 }
 
-export async function getAllDishsPictures({
+export async function getAllDishsServiceItems({
   pathnameParams,
   dishs,
   pictures,
+  extras,
 }: {
   pathnameParams: string;
   dishs?: boolean;
   pictures?: boolean;
+  extras?: boolean;
 }) {
   return await prisma.dishService.findFirst({
     where: {
@@ -21,6 +23,7 @@ export async function getAllDishsPictures({
     include: {
       dishs: dishs,
       pictures: pictures,
+      extras: extras,
     },
   });
 }
