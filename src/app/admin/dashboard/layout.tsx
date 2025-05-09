8d100a203bb9143315d53c9dcbfcd5525517a452
Gmayer111 <gaelmayer@yahoo.fr>
2025-05-09 11:09:52 +0200

    feat: move providers to root admin layout

--- src/app/admin/dashboard/layout.tsx
+++ src/app/admin/dashboard/layout.tsx
@@ -8,42 +8,39 @@ import DashboardSidenav, {
   TMenuItems,
 } from "src/components/dashboard/dashboard-sidenav/dashboard-sidenav.component";
 import { getAllDishServices } from "src/app/actions/dishs-service/dish-services.actions";
-import { ToastProvider } from "../../providers/toast.provider";
 
 const DashboardLayout = async ({ children }: { children: ReactNode }) => {
   const dishServices = await getAllDishServices();
 
-  if (!dishServices) return <div>Chargement...</div>;
+  if (!dishServices) return <div> ...</div>;
 
-  // const sidenavItems: TMenuItems[] = [
-  //   {
-  //     menuItems: [
-  //       {
-  //         content: "Restaurant",
-  //         path: `/fr/carte`,
-  //         icon: <ArrowRightEndOnRectangleIcon />,
-  //       },
-  //     ],
-  //   },
-  //   {
-  //     menuItems: dishServices.map((dishService, index) => {
-  //       let alternateIcon = index % 2;
-  //       return {
-  //         content: dishService.titleFR,
-  //         path: `/admin/dashboard/${dishService.slug}/dishs`,
-  //         icon: alternateIcon === 0 ? <FaceSmileIcon /> : <HeartIcon />,
-  //       };
-  //     }),
-  //   },
-  // ];
+  const sidenavItems: TMenuItems[] = [
+    {
+      menuItems: [
+        {
+          content: "Restaurant",
+          path: `/fr/carte`,
+          icon: <ArrowRightEndOnRectangleIcon />,
+        },
+      ],
+    },
+    {
+      menuItems: dishServices.map((dishService, index) => {
+        let alternateIcon = index % 2;
+        return {
+          content: dishService.titleFR,
+          path: `/admin/dashboard/${dishService.slug}/dishs`,
+          icon: alternateIcon === 0 ? <FaceSmileIcon /> : <HeartIcon />,
+        };
+      }),
+    },
+  ];
 
   return (
-    <ToastProvider>
-      <div className="dashboard-layout">
-        <DashboardSidenav sidenavItems={sidenavItems} />
-        {children}
-      </div>
-    </ToastProvider>
+    <div className="dashboard-layout">
+      <DashboardSidenav sidenavItems={sidenavItems} />
+      {children}
+    </div>
   );
 };
 
