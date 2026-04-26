8fd43fdb501b738da1e4e77b051fc6d9361aedbe
Gmayer111 <gaelmayer@yahoo.fr>
2026-04-26 13:42:25 +0200

    feat: create update title action

--- src/app/actions/dishs-service/dish-services.actions.ts
+++ src/app/actions/dishs-service/dish-services.actions.ts
@@ -1,12 +1,25 @@
 "use server";
+import { revalidatePath } from "next/cache";
+import { Prisma } from "prisma/generated/prisma";
 import prisma from "src/lib/db";
 
 export async function getAllDishServices() {
-  return await prisma.dishService.findMany({});
+  return await prisma.dishService.findMany({
+    orderBy: [
+      {
+        id: "asc",
+      },
+    ],
+  });
 }
 
 export async function getAllDishServicesMenuItems() {
   return await prisma.dishService.findMany({
+    orderBy: [
+      {
+        id: "asc",
+      },
+    ],
     include: {
       dishs: true,
       pictures: true,
@@ -45,4 +58,27 @@ export async function getAllDishsServiceItems({
       },
     },
   });
+}
+
+export async function updateDishServiceTitle(
+  id: number,
+  formData: FormData,
+  path?: string,
+) {
+  try {
+    await prisma.dishService.update({
+      where: {
+        id,
+      },
+      data: {
+        titleEN: formData.get("titleEN") as string,
+        titleFR: formData.get("titleFR") as string,
+      },
+    });
+    revalidatePath(`/admin/dahsboard/${path}`);
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      return { error: error.message };
+    }
+  }
 }
